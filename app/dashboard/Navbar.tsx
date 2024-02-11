de571eefc206741de24635fe9bd12948eb378d5f
Timur Kramár <timurjan.kramar123@gmail.com>
2024-02-11 18:08:31 +0100

    fix: link href type safety

--- app/dashboard/Navbar.tsx
+++ app/dashboard/Navbar.tsx
@@ -8,6 +8,32 @@ import { Users } from "@/utils/supabase/database.types";
 import { HiCalendarDays } from "react-icons/hi2";
 import { logOutClient } from "../auth/clientActions";
 import { useStoreContext } from "./store";
+import { UrlObject } from "url";
+import { Route } from "next";
+
+type route = {
+  title: string;
+  href: Route;
+  icon: JSX.Element;
+};
+
+const routes: route[] = [
+  {
+    title: "Predstavenia",
+    href: "/dashboard/services",
+    icon: <FaTheaterMasks className="h-5 w-5" />,
+  },
+  {
+    title: "Udalosti",
+    href: "/dashboard/events",
+    icon: <HiCalendarDays className="h-5 w-5" />,
+  },
+  {
+    title: "Kupóny",
+    href: "/dashboard/coupons",
+    icon: <FaTicketAlt className="h-5 w-5" />,
+  },
+];
 
 export default function Navbar({ profile }: { profile?: Users }) {
   const path = usePathname();
@@ -18,25 +44,9 @@ export default function Navbar({ profile }: { profile?: Users }) {
       <p className="hidden px-4 text-lg font-bold tracking-wider md:inline">
         {profile?.business_name || "No name"}
       </p>
-      {[
-        {
-          title: "Predstavenia",
-          href: "/dashboard/services",
-          icon: <FaTheaterMasks className="h-5 w-5" />,
-        },
-        {
-          title: "Udalosti",
-          href: "/dashboard/events",
-          icon: <HiCalendarDays className="h-5 w-5" />,
-        },
-        {
-          title: "Kupóny",
-          href: "/dashboard/coupons",
-          icon: <FaTicketAlt className="h-5 w-5" />,
-        },
-      ].map((item) => (
+      {routes.map((item) => (
         <Link
-          key={item.href}
+          key={item.title}
           className={`grid w-auto place-content-center rounded-lg p-2 md:block md:px-3 md:py-0.5 ${
             path.startsWith(`${item.href}`)
               ? "bg-white font-bold tracking-widest text-black hover:bg-gray-50"
