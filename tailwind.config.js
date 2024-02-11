0054792d9ac22ce3eb4b10c3d09ace9846f4d052
Timur Kramár <timurjan.kramar123@gmail.com>
2024-02-11 15:17:54 +0100

    fix: tailwind include utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite/**/*.js",
     "./node_modules/flowbite-react/lib/**/*.js",
   ],
